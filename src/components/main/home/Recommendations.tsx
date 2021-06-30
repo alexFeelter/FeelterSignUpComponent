871c68d16435c45ce3f5a4f88308d832778728b7
alex <alex@feelter.com>
2021-06-30 17:20:01 +0300

    cleaned a code

--- src/components/main/home/Recommendations.tsx
+++ src/components/main/home/Recommendations.tsx
@@ -5,21 +5,20 @@ import { RecommendedNextAction } from "../RecommendedNextAction"
 import { RecommendationsState } from "./Home"
 
 export const Recommendations = props => {
-	console.log("props ", props)
 	return <section>
 		<div>
-		<h2>{ `${ props.home.recomendations.welcome} Line,` }</h2>
-		<p>{ props.home.recomendations.welcomeDescription }</p>
-		<div>
-			<h4>{ props.home.recomendations.capture }</h4>
-			{ props.home.recomendations.welcome && props.home.recomendations.data.map(recom => 
-			<RecommendedNextAction 
-				recomCapture={ recom.capture }
-				recomDescription={ recom.captureDescription }
-				durationAction={ recom.durationAction }
-				buttonLabel={ recom.buttonLabel }
-			/>) }
-		</div>
+			<h2>{ `${ props.home.recomendations.welcome} Line,` }</h2>
+			<p>{ props.home.recomendations.welcomeDescription }</p>
+			<div>
+				<h4>{ props.home.recomendations.capture }</h4>
+				{ props.home.recomendations.welcome && props.home.recomendations.data.map(recom => 
+				<RecommendedNextAction 
+					recomCapture={ recom.capture }
+					recomDescription={ recom.captureDescription }
+					durationAction={ recom.durationAction }
+					buttonLabel={ recom.buttonLabel }
+				/>) }
+			</div>
 		</div>
 	</section>
 }
